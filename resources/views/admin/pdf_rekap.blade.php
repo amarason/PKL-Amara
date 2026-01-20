5f54706a557faa2cf77f58adc2ddcd9f0203c4f8
sonamara <amaraputrisoniaji@students.undip.ac.id>
2026-01-20 09:55:03 +0700

    penyamaan header rekap laporan pdf

--- resources/views/admin/pdf_rekap.blade.php
+++ resources/views/admin/pdf_rekap.blade.php
@@ -16,7 +16,8 @@
                 <td class="header-text">
                     <h1>PT PLN INDONESIA POWER</h1>
                     <h2>UBP SEMARANG</h2>
-                    <p>Laporan Rekapitulasi Absensi Seluruh Peserta PKL</p>
+                    <p>Laporan Rekapitulasi Absensi Peserta Praktek Kerja Lapangan (PKL)</p>
+                    <p class="sub-p">Sistem Informasi Praktek Kerja (SIPRAKER)</p>
                 </td>
             </tr>
         </table>
